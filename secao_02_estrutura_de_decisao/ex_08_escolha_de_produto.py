844896e794b2f38f211140bf9cca33c7873b285c
Marcelo Magario <marcelomagario@gmail.com>
2022-08-05 16:07:41 +1000

    Solução do exercício 8

--- secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
+++ secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
@@ -19,4 +19,6 @@ Mostrar o resultado com duas casas decimais
 
 
 def decidir_melhor_produto(x, y, z):
-    """Escreva aqui em baixo a sua solução"""
+    """Escreva aqui em baixo a sua solução"""   
+    mais_barato = min(x, y, z)
+    print(f'Melhor produto custa R$ {mais_barato:.2f}')
