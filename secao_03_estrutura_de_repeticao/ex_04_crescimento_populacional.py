7d22ee6e2d002d5835206f6e1a9966080a784e59
Marcelo Magario <marcelomagario@gmail.com>
2022-08-13 17:32:51 +1000

    Solução do exercício 4

--- secao_03_estrutura_de_repeticao/ex_04_crescimento_populacional.py
+++ secao_03_estrutura_de_repeticao/ex_04_crescimento_populacional.py
@@ -16,3 +16,20 @@ de crescimento.
 
 def calcular_ano_ultrapassagem_populacional() -> str:
     """Escreva aqui em baixo a sua solução"""
+    populacao_a = 80_000
+    taxa_a = 0.03
+    populacao_b = 200_000
+    taxa_b = 0.015
+    qtde_de_anos = 0
+    while populacao_b > populacao_a:
+        aumento_anual_a = populacao_a * taxa_a
+        populacao_a = populacao_a + aumento_anual_a
+        aumento_anual_b = populacao_b * taxa_b
+        populacao_b = populacao_b + aumento_anual_b
+        qtde_de_anos = qtde_de_anos + 1
+    else:
+        return (
+                f'População de A, depois de {qtde_de_anos} ano(s) será de {round(populacao_a)} pessoas,'
+                f' superando a de B, que será de {round(populacao_b)} pessoas'
+        )
+
