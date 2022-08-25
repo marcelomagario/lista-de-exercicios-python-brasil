2b0903fb02a5571b771c2cfb678b673f17f10a24
Marcelo Magario <marcelomagario@gmail.com>
2022-08-25 14:24:13 +1000

    Solução do exercício 23

--- secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
+++ secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
@@ -43,13 +43,47 @@ from typing import Tuple
 
 def calcular_primos_e_divisoes(n: int) -> Tuple[str, int]:
     """Escreva aqui em baixo a sua solução"""
-    if n < 2:
-        return False
-    elif n ==2:
-        return True
-    for i in range(2, n):
-        if (n % i) == 0:
-            return False
-    else:
-        return True
+
+    # Usada solução do Crivo de Erastótenes
+
+    primos = []
+    divisoes = 0
+    if n > 1:
+        candidatos_a_primos = list(range(3, n + 1, 2))
+        candidatos_a_primos.reverse()
+        candidatos_a_primos.append(2)
+        while len(candidatos_a_primos) > 0:
+            primo = candidatos_a_primos.pop()
+            primos.append(primo)
+            divisoes += len(candidatos_a_primos)
+            candidatos_a_primos = [n for n in candidatos_a_primos if n % 2 != 0]
+
+    return ', '.join(map(str, primos)), divisoes
+
+#  Minha solução usando a reaproveitando a função do exercício anterior.
+# intervalo = 100
+# numeros_primos = []
+# qtde_nao_primos = 0
+#
+# def eh_primo(n: int) -> bool:
+#     """Escreva aqui em baixo a sua solução"""
+#     if n < 2:
+#         return False
+#     elif n ==2:
+#         return True
+#     for i in range(2, n):
+#         if (n % i) == 0:
+#             return False
+#     else:
+#         return True
+#
+# for i in range(intervalo):
+#     if eh_primo(i):
+#         numeros_primos.append(i)
+#     else:
+#         qtde_nao_primos += 1
+#
+# print(intervalo)
+# print(numeros_primos) # Imprimi todos os números primos do intervalo.
+# print(qtde_nao_primos)
 
