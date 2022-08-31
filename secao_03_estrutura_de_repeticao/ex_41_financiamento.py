1dbaf18b5fe3f49f44a916b74990c7b68bd9bd1c
Marcelo Magario <marcelomagario@gmail.com>
2022-08-31 22:44:37 +1000

    Solução do exercício 41

--- secao_03_estrutura_de_repeticao/ex_41_financiamento.py
+++ secao_03_estrutura_de_repeticao/ex_41_financiamento.py
@@ -32,3 +32,26 @@ Quantidade de Parcelas  % de Juros sobre o valor inicial da dívida
 
 def gerar_dados_de_financiamente(valor_inicial: float):
     """Escreva aqui em baixo a sua solução"""
+    divida_10 = valor_inicial * 1.1
+    divida_15 = valor_inicial * 1.15
+    divida_20 = valor_inicial * 1.2
+    divida_25 = valor_inicial * 1.25
+
+    valor_da_parcela_3 = divida_10 / 3
+    valor_da_parcela_6 = divida_15 / 6
+    valor_da_parcela_9 = divida_20 / 9
+    valor_da_parcela_12 = divida_25 / 12
+
+    nova_entrada = [(valor_inicial, '0%', '1', valor_inicial),
+                    (divida_10, '10%', '3', valor_da_parcela_3),
+                    (divida_15, '15%', '6', valor_da_parcela_6),
+                    (divida_20, '20%', '9', valor_da_parcela_9),
+                    (divida_25, '25%', '12', valor_da_parcela_12)]
+
+
+    print("{:<2} {:<1} {:<1}  {:<1}".format('Valor da Dívida', 'Valor dos Juros', 'Quantidade de Parcelas', 'Valor da Parcela'))
+
+
+    for n in nova_entrada:
+        divida, porcentagem, qtde_parcelas, valor_parcelas = n
+        print("R$ {:<12.2f} {:<15} {:<23} R${:>10.2f}".format(divida, porcentagem, qtde_parcelas, valor_parcelas))
