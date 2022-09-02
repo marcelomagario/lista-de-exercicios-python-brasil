1a09981c370da51b57289750f8d6b3b42b363833
Marcelo Magario <marcelomagario@gmail.com>
2022-09-02 19:30:13 +1000

    Solução do exercício 45

--- secao_03_estrutura_de_repeticao/ex_45_corretor_de_provas.py
+++ secao_03_estrutura_de_repeticao/ex_45_corretor_de_provas.py
@@ -49,3 +49,39 @@ Gabarito da Prova:
 
 def corrigir(*provas):
     """Escreva aqui em baixo a sua solução"""
+    gabarito = {
+        1: 'A',
+        2: 'B',
+        3: 'C',
+        4: 'D',
+        5: 'E',
+        6: 'E',
+        7: 'D',
+        8: 'C',
+        9: 'B',
+        10: 'A',
+    }
+
+    alunos_notas = {}
+    maior_nota, menor_nota, total_alunos, media_nota = 0, 0, 0, 0
+    for i in range(len(provas)):
+        nota_contador = 0
+        for n in range(1, len(provas[i])):
+            # Verifica e contabiliza se a entrada é igual a resposta no gabarito.
+            if provas[i][n] == gabarito.get(n):
+                nota_contador += 1
+        # Salva o nome e a nota do aluno no dicionário
+        alunos_notas[provas[i][0]] = nota_contador
+    maior_nota = max(alunos_notas.values())
+    menor_nota = min(alunos_notas.values())
+    total_alunos = len(alunos_notas)
+    media_nota = (maior_nota + menor_nota) / 2
+    print('Aluno                 Nota')
+    for k, v in alunos_notas.items():
+        print(f'{k:21s} {v:2d}')
+
+    print('---------------------------')
+    print(f'Média geral: {media_nota}')
+    print(f'Maior nota: {maior_nota}')
+    print(f'Menor nota: {menor_nota}')
+    print(f'Total de Alunos: {total_alunos}')
