2bc5f89c1782e9bb4b8b36992c5e7ef28aba9199
Marcelo Magario <marcelomagario@gmail.com>
2022-09-03 15:26:27 +1000

    Solução do exercício 01 - lista 4

--- secao_04_exercicios_lista/ex_01_vetor_5_numeros.py
+++ secao_04_exercicios_lista/ex_01_vetor_5_numeros.py
@@ -7,8 +7,10 @@ Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
     >>> ler_5_valores()
 
 """
-
-
 def ler_5_valores():
     """Escreva aqui em baixo a sua solução"""
+    vetor = []
+    for i in range(0, 5):
+        vetor += [int(input('Digite 1 número inteiro: '))]
+    print(vetor)
 
