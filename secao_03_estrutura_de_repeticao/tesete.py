ea414644f7efb998b4649cc8a297005c93348e5d
Marcelo Magario <marcelomagario@gmail.com>
2022-09-03 17:43:40 +1000

    Troquei o exercício 6 pelo 9 - Lista 4 - erro no DevPro

--- secao_03_estrutura_de_repeticao/tesete.py
+++ secao_03_estrutura_de_repeticao/tesete.py
@@ -1,19 +1,30 @@
-numero = 223
-centenas_msg = dezenas_msg = unidades_msg = ''
+"""
+Exercício 06 da seção de listas da Python Brasil:
+https://wiki.python.org.br/ExerciciosListas
 
-if numero <= 0:
-    print('O número precisa ser positivo')
-if numero > 1000:
-    print('O número precisa ser menor que 1000')
+Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
+imprima o número de alunos com média maior ou igual a 7.0, e imprima separado o número de alunos com nota menor que 7.0 .
 
-centenas_numero = numero // 100
-centenas_numero, numero = divmod(numero, 100)
+    >>> calcular_media([8,9,10,2],[2,10,9,5],[5,2,9,10],[10,1,1,10],[9,2,9,8],[8,8,9,7],[7,7,7,9],[4,6,5,7],[1,2,8,10],[10,2,2,8])
+    Alunos com media >= 7.0: 4
+    Alunos com media < 7.0: 6
+    >>> calcular_media([2,1,1,2],[2,1,0,4],[3,2,5,2],[9,7,7,9],[0,6,9,2],[9,1,6,4],[10,10,9,7],[6,7,8,9],[9,8,9,6],[10,5,10,8])
+    Alunos com media >= 7.0: 5
+    Alunos com media < 7.0: 5
+"""
+
+from statistics import mean
+def calcular_media(*notas) -> int:
+    """Escreva aqui em baixo a sua solução"""
+    acima_da_media, abaixo_da_media = 0, 0
+
+    for aluno in range(len(notas)):
+        media = mean(notas[aluno])
+        if media >= 7:
+            acima_da_media += 1
+        else:
+            abaixo_da_media += 1
+    print(f'Alunos com media >= 7.0: {acima_da_media}')
+    print(f'Alunos com media < 7.0: {abaixo_da_media}')
 
 
-if centenas_numero == 1:
-    centenas_msg = '1 centena'
-if centenas_numero > 1:
-    centenas_msg = f'{centenas_numero} centenas'
-
-
-print(centenas_msg)
--- secao_04_exercicios_lista/ex_06_calculador_de_media.py
+++ secao_04_exercicios_lista/ex_06_calculador_de_media.py
@@ -1,30 +1,26 @@
 """
-Exercício 06 da seção de listas da Python Brasil:
+Exercício 09 da seção de listas da Python Brasil:
 https://wiki.python.org.br/ExerciciosListas
 
-Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
-imprima o número de alunos com média maior ou igual a 7.0, e imprima separado o número de alunos com nota menor que 7.0 .
+Faça um Programa que receba um vestor de números inteiros, calcule e mostre a soma dos quadrados dos elementos do
+vetor.
 
-    >>> calcular_media([8,9,10,2],[2,10,9,5],[5,2,9,10],[10,1,1,10],[9,2,9,8],[8,8,9,7],[7,7,7,9],[4,6,5,7],[1,2,8,10],[10,2,2,8])
-    Alunos com media >= 7.0: 4
-    Alunos com media < 7.0: 6
-    >>> calcular_media([2,1,1,2],[2,1,0,4],[3,2,5,2],[9,7,7,9],[0,6,9,2],[9,1,6,4],[10,10,9,7],[6,7,8,9],[9,8,9,6],[10,5,10,8])
-    Alunos com media >= 7.0: 5
-    Alunos com media < 7.0: 5
+    >>> calcular_soma_de_quadrados([])
+    0
+    >>> calcular_soma_de_quadrados([1])
+    1
+    >>> calcular_soma_de_quadrados([1, 2])
+    5
+    >>> calcular_soma_de_quadrados(list(range(10)))
+    285
+
 """
-
-from statistics import mean
-def calcular_media(*notas) -> int:
+def calcular_soma_de_quadrados(inteiros: list) -> int:
     """Escreva aqui em baixo a sua solução"""
-    acima_da_media, abaixo_da_media = 0, 0
-
-    for aluno in range(len(notas)):
-        media = mean(notas[aluno])
-        if media >= 7:
-            acima_da_media += 1
-        else:
-            abaixo_da_media += 1
-    print(f'Alunos com media >= 7.0: {acima_da_media}')
-    print(f'Alunos com media < 7.0: {abaixo_da_media}')
-
-
+    lista_quadrados = []
+    for i in range(len(inteiros)):
+        quadrado = inteiros[i] ** 2
+        lista_quadrados.append(quadrado)
+    soma = sum(lista_quadrados)
+    print(soma)
+# marcelo
