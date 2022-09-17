1aba94985a0a5ffbf66f1e4f6cc763d06e795ea5
Marcelo Magario <marcelomagario@gmail.com>
2022-09-17 14:31:44 +1000

    Solução do Exercício 8 - lista 5

--- secao_05_exercicios_funcoes/ex_08_quantidade_de_digitos.py
+++ secao_05_exercicios_funcoes/ex_08_quantidade_de_digitos.py
@@ -23,16 +23,11 @@ Faça uma função que informe a quantidade de dígitos de um determinado númer
     1
 
 """
-def imprimir_qtde_digitos_do_inteiro(n: int) -> int:
-    if not isinstance(n, int):
-        print('O valor informado não é um inteiro')
-        return
-    return len(str(n).replace('-', ''))
 
-# def imprimir_qtde_digitos_do_inteiro(n: int) -> int:
-#     if type(n) != int:
-#         print('O valor informado não é um inteiro')
-#     else:
-#         list = [int(i) for i in str(n).replace('-', '')]
-#         print(len(list))
-#
+def imprimir_qtde_digitos_do_inteiro(n: int) -> int:
+    if type(n) != int:
+        print('O valor informado não é um inteiro')
+    else:
+        list = [int(i) for i in str(n).replace('-', '')]
+        print(len(list))
+
